e5b424b1095e8e820de5408a9879537cd4d4c972
root <root@L-Yisus.localdomain>
2025-06-27 14:01:14 -0400

    FROMUNDA MAN LIVESgit statusgit status Fromunda Man has been implemented: battle and overworld Sprite. To be tested if Trainer Class functions.

--- src/battle_main.c
+++ src/battle_main.c
@@ -373,6 +373,7 @@ const struct TrainerClass gTrainerClasses[TRAINER_CLASS_COUNT] =
     [TRAINER_CLASS_PIKE_QUEEN] = { _("PIKE QUEEN") },
     [TRAINER_CLASS_PYRAMID_KING] = { _("PYRAMID KING") },
     [TRAINER_CLASS_RS_PROTAG] = { _("{PKMN} TRAINER") },
+    [TRAINER_CLASS_FROMUNDA_MAN] = { _("FROMUNDA MAN"), 30 },
 };
 
 static void (* const sTurnActionsFuncsTable[])(void) =
